ed8b8947554d23fcfefc601dc70ed93d41e6d618
mike crane <mike.c0321@gmail.com>
2018-04-26 10:27:25 -0400

    create tests for GET, POST, PUT, and DELETE requests to /recipes

---
+++ test/test-recipe.js
@@ -0,0 +1,50 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { app, runServer, closeServer } = require('../server');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Recipes', function () {
+
+  before(function () {
+    return runServer();
+  });
+
+  after(function () {
+    return closeServer();
+  });
+
+
+  it('should list recipes on GET', function () {
+
+    return chai.request(app)
+      .get('/recipes')
+      .then(function (res) {
+
+        res.should.have.status(200);
+        res.should.be.json;
+
+        res.body.should.be.a('array');
+        res.body.should.have.length.of.at.least(1);
+        res.body.forEach(function (item) {
+          item.should.be.a('object');
+          item.should.include.keys('id', 'name', 'ingredients');
+        });
+      });
+  });
+
+  
+  it('should delete recipes on DELETE', function () {
+
+    return chai.request(app)
+      .get('/recipes')
+      .then(function (res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`)
+      })
+      .then(function (res) {
+        res.should.have.status(204);
+      });
+  });
+});
